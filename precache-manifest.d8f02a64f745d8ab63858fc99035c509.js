992788956bba5150c8a811081e1a5db4059e64db
Frankie Riviera <frankgriviera@outlook.com>
2019-09-27 13:59:07 -0400

    Updates

---
+++ precache-manifest.d8f02a64f745d8ab63858fc99035c509.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ba2084fd3b438c15b9f7db9c3496b8",
+    "url": "/fgr-video/index.html"
+  },
+  {
+    "revision": "2b62165206cf1e4eed74",
+    "url": "/fgr-video/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "31f90f8f590aaca46bd8",
+    "url": "/fgr-video/static/js/2.6f815b4a.chunk.js"
+  },
+  {
+    "revision": "2b62165206cf1e4eed74",
+    "url": "/fgr-video/static/js/main.85f8461d.chunk.js"
+  },
+  {
+    "revision": "03791a75489637b7618a",
+    "url": "/fgr-video/static/js/runtime-main.2091e7d7.js"
+  }
+]);

573ab62844a9a1487f01aa725033176507fe4b97
Frankie Riviera <frankgriviera@outlook.com>
2019-11-09 15:32:50 -0500

    Updates

---
+++ precache-manifest.dbf51ae8211890582d2eb969b6541829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d396544671bd28674ab87f5baa95d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61e935c904c26bc3cfce",
+    "url": "/static/css/main.7473f7be.chunk.css"
+  },
+  {
+    "revision": "31f90f8f590aaca46bd8",
+    "url": "/static/js/2.6f815b4a.chunk.js"
+  },
+  {
+    "revision": "61e935c904c26bc3cfce",
+    "url": "/static/js/main.4200b75e.chunk.js"
+  },
+  {
+    "revision": "8fe7c5b4278752c22376",
+    "url": "/static/js/runtime-main.b027bd41.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fgr-video/precache-manifest.010b2520bdf8d811bd06c95d26c1d2a7.js"
+  "/precache-manifest.dbf51ae8211890582d2eb969b6541829.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fgr-video/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

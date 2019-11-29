279e40ee7c0ba8e9c56ce4c00371b9f3e17c4484
Frankie Riviera <frankgriviera@outlook.com>
2019-11-29 14:53:49 -0500

    aded 2 to the portfolio page title

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -5,7 +5,7 @@ export class PortfolioPage extends Component {
     render() {
         return (
             <div>
-                <h1>MY PortfolioPage</h1>
+                <h1>MY PortfolioPage 2</h1>
                 <Link to="/">HomePage</Link>
             </div>
         )

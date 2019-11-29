3f39d1988adbcad9bad75f1e035b08e3fe8bcb47
Frankie Riviera <frankgriviera@outlook.com>
2019-11-29 15:07:29 -0500

    added cname to public

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -5,7 +5,7 @@ export class PortfolioPage extends Component {
     render() {
         return (
             <div>
-                <h1>MY PortfolioPage 2</h1>
+                <h1>MY PortfolioPage with cname in public folder</h1>
                 <Link to="/">HomePage</Link>
             </div>
         )

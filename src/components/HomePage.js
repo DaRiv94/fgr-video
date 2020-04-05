186b6328d50207315d25eb23ca644cf9b089829d
DaRiv94 <frankgriviera@outlook.com>
2020-04-05 16:06:25 -0400

    added styling to fgrvideo.com

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {Link} from "react-router-dom";
+import "./HomePage.css"
 
 export class HomePage extends Component {
     render() {
@@ -9,16 +10,28 @@ export class HomePage extends Component {
                     <h1>
                         FGR Video
                     </h1>
+                    <p>FGR Video is currently in develpment</p>
+
+                    <Link className="App-link" to="/portfolio">Demo Reel</Link>
+
                     <a
                         className="App-link"
-                        href="http://frankieriviera.com"
+                        href="https://frankieriviera.com"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
                         FrankieRiviera.com
                     </a>
+                    <a
+                        className="App-link"
+                        href="https://legacy.fgrvideo.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Legacy FGRVideo.com
+                    </a>
 
-                    <Link to="/portfolio">Portfolio</Link>
+
                 </header>
             </div>
         )
--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./PortfolioPage.css"
 
 export class PortfolioPage extends Component {
     render() {
         return (
-            <div>
-                <h1>MY PortfolioPage with cname in public folder</h1>
-                <Link to="/">HomePage</Link>
+            <div className="portfolio-page">
+                <h1>FGR Video Demo Reel</h1>
+                <Link className="App-link" to="/">Home</Link>
+                
+                    <div className="portfolioDemoReel" >
+                        <iframe title="DemoReel" className="DemoReel" src="https://www.youtube.com/embed/DzJR1_381G0" ></iframe>
+                    </div>
+                
+                {/* <div className="DemoReelBox">
+                    <div className="portfolioDemoReel" >
+                        <iframe title="DemoReel" className="DemoReel" src="https://www.youtube.com/embed/DzJR1_381G0" ></iframe>
+                    </div>
+                </div> */}
             </div>
         )
     }
